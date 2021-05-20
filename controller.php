aea3d3b3eb8a0db9c341c7864dc5d8b3df963ebd
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-05-20 07:43:11 +0700

    initial commit

---
+++ controller.php
@@ -0,0 +1,16 @@
+<?php
+    function connect_database($connect){
+        $dbhost = "localhost";
+        $dbuser = "root";
+        $dbpass = "";
+        $db = "project";
+
+        $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $db) or die ("Error  Connecting to Database");
+
+        return $connect;
+    }
+
+    function close_connection($x){
+        mysqli_close($x);
+    }
+?>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="round1"></div>
+    <div>
+        <header>
+            <h1>GameInfo</h1>
+            <h3>INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h3>
+        </header>
+    </div>
+    <div class="possition">
+        <div class="round2"></div>
+    </div>
+    
+    <div class="loginInput">
+        <form action="" method="POST">
+            <label for="username">Username / Email</label><br>
+            <input class="input" type="text" name="username"><br><br><br>
+            <label for="password">Password</label><br>
+            <input class="input" type="password" name="password"><br><br>
+            <input class="submit" type="submit" name="login" value="Login">
+
+        </form>
+    </div>
+
+</body>
+
+</html>
