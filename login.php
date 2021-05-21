b234b559290fbba182b0386090533f18b2cad626
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-05-21 23:43:55 +0700

    initial commit

--- login.php
+++ login.php
@@ -17,14 +17,11 @@
             <h3>INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h3>
         </header>
     </div>
-    <div class="possition">
-        <div class="round2"></div>
-    </div>
-
+    <div class="round2"></div>
     <div class="loginInput">
         <form action="" method="POST">
             <label for="username">Username / Email</label><br>
-            <input class="input" type="text" name="username"><br><br><br>
+            <input class="input" type="text" name="username"><br>
             <label for="password">Password</label><br>
             <input class="input" type="password" name="password"><br><br>
             <input class="submit" type="submit" name="login" value="Login">
