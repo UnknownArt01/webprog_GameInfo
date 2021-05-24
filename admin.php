031cd11c187440179c840b078bfde41b7999dc2a
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-05-24 09:37:52 +0700

    entahlah

--- admin.php
+++ admin.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="all.css">
+    <title>admin</title>
 </head>
-<body>
-    
+
+<body>//kategori id sama sama nama
+    <div>
+        <header class="adminHeader">
+            <div>
+                <h1 class="adminHeaderLogo">GameInfo</h1>
+                <div></div>
+                <h4 class="adminHeaderLogo2">INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h4>
+            </div>
+            <div>
+                <a href="">Home</a>
+                <a href="">Category</a>
+                <a href="">News</a>
+                <a href="">HOT NEWS!</a>
+                <a href="">Admin</a>
+            </div>
+            <div>
+               <form action="">
+                   <input class="inputSearch" type="text" name="search" value="Search">
+               </form> 
+            </div>
+        </header>
+    </div>
 </body>
+
 </html>
