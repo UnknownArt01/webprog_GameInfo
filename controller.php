2900d4da61787a0329ba9b9ec1983d7e5ee641a3
MakLo1337 <mrwinataa@gmail.com>
2021-05-24 09:35:16 +0700

    login ui, php and css update

--- controller.php
+++ controller.php
@@ -13,4 +13,28 @@
     function close_connection($x){
         mysqli_close($x);
     }
+
+    function login(){
+        $adminuser = "admin";
+        $adminpass = "admin";
+
+        session_start();
+
+        $user = isset($_POST['username']) ? $_POST['username'] : "";
+        $pass = isset($_POST['password']) ? $_POST['password'] : "";
+
+        if($user == $adminuser && $pass == $adminpass){
+            session_start();
+            $_SESSION['username'] = $user;
+            header("Location: admin.php");
+        } else {
+            $message = "Invalid Login Credentials!";
+            echo "<script>
+            alert('$message');
+            window.location.href='login.php';
+            </script>";
+            exit;
+            // header("Location: login.php");
+        }
+    }
 ?>
