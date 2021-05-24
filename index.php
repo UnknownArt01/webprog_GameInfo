e7c37d6c84d55634bfcdc53cdac8fa1cbacacf81
michaellchandra <76256177+michaellchandra@users.noreply.github.com>
2021-05-24 09:05:46 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"> -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    
+    <title>GameInfo Official Website</title>
+</head>
+<body>
+
+    <!--Navigation Bar -->
+    <nav>
+        <div class="logo">
+            <a class="logo" href="#"><h4>GameInfo</h4></a>
+        </div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Category</a></li>
+            <li><a href="#">News</a></li>
+            <li><a class="hotnews" href="#">HOT NEWS!</a></li>
+        </ul>
+
+        <div class="search-box">
+            <form action="#">
+                <input type="text" placeholder="Search.." name="search">
+                <button type="submit">Search</button>
+            </form>
+
+        </div>
+
+
+    </nav>
+
+    
+    <!--Halaman Utama -->
+    <section class="container1">
+        <div class="div1-left">
+
+        </div>
+        
+    </section>
+
+    <section class="container2">
+        <div class="latest-post">
+            <h1>LATEST POST</h1>
+            <br>
+
+        </div>
+
+        <div class="our-social-media">
+            <h1>OUR SOCIAL MEDIA!</h1>
+
+            <div class="socmed-facebook">
+                <h1>Facebook</h1>
+            </div>
+
+            <div class="socmed-youtube">
+                <h1>Youtube</h1>
+            </div>
+
+            <div class="socmed-instagram">
+                <h1>Instagram</h1>
+            </div>
+
+
+        </div>
+    
+    </section>
+    
+    <!-- <section>
+        <footer>
+            <h4>Copyright 2021 | GameInfo</h4>
+        </footer>
+
+    </section> -->
+
+           
+
+</body>
+</html>
