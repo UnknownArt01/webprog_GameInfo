323f2dfb5851224b123ff20c900e90adda9bc13e
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-05-25 14:15:32 +0700

    update footer and header

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,99 +9,113 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <!-- <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet"> -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&family=Montserrat:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&family=Montserrat:wght@500&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
-    
+    <link rel="stylesheet" href="all.css">
+
     <title>GameInfo Official Website</title>
 </head>
+
 <body>
-
-    <!--Navigation Bar -->
-    <nav>
-        <div class="logo">
-            <a class="logo" href="#"><h4>GameInfo</h4></a>
-        </div>
-
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Category</a></li>
-            <li><a href="#">News</a></li>
-            <li><a class="hotnews" href="#">HOT NEWS!</a></li>
-        </ul>
-
-        <div class="search-box">
-            <form action="#">
-                <input type="text" placeholder="Search.." name="search">
-                <button type="submit">Search</button>
-            </form>
-
-        </div>
-
-
-    </nav>
-
-    
-    <!--Halaman Utama -->
-    <section class="container1">
-        <div class="div1-left">
-
-        </div>
-        
-    </section>
-
-    <section class="container2">
-        <div class="latest-post">
-            <h1>LATEST POST</h1>
-            <br>
-            <div class="latest-post1">
-                <h2>Selyandaru Akhirnya Tidak Jomblo</h2>
-                <p>lorem</p>
+    <div class="bodydiv">
+        <!--Navigation Bar -->
+        <header class="adminHeader">
+            <div class="header">
+                <h1 class="adminHeaderLogo">GameInfo</h1>
+                <div class="clear"></div>
+                <h4 class="adminHeaderLogo2">INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h4>
+            </div>
+            <div class="a_header">
+                <div class="adminHeaderA">
+                    <a class="a_header" href="">Home</a>
+                    <a class="a_header" href="">Category</a>
+                    <a class="a_header" href="">News</a>
+                    <a class="a_header" href="">HOT NEWS!</a>
+                    <a class="a_header" href="">Admin</a>
+                </div>
             </div>
 
-            <div class="latest-post2">
-                <h2>Rafi Masuk Nominal Tertampan IMT?</h2>
-                <p>lorem</p>
+            <div class="header">
+                <form action="">
+                    <input class="inputSearch" type="text" name="search" value="Search">
+                </form>
             </div>
-                 
-            <div class="latest-post3">
-                <h2>Seorang Programmer Terkena Tipes</h2>
-                <p>lorem</p>
-            </div>
+        </header>
+        <!--Halaman Utama -->
+        <section class="container1">
+            <div class="div1-left">
 
-        </div>
-
-        <div class="our-social-media">
-            <h1>OUR SOCIAL MEDIA!</h1>
-
-            <div class="socmed-facebook">
-                <img src="img/facebook.png" class="img-fb">
-                <h1>Facebook</h1>
             </div>
 
-            <div class="socmed-youtube">
-                <img src="img/youtube.png" class="img-yt">
-                <h1>Youtube</h1>
+        </section>
+
+        <section class="container2">
+            <div class="latest-post">
+                <h1>LATEST POST</h1>
+                <br>
+                <div class="latest-post1">
+                    <h2>Selyandaru Akhirnya Tidak Jomblo</h2>
+                    <p>lorem</p>
+                </div>
+
+                <div class="latest-post2">
+                    <h2>Rafi Masuk Nominal Tertampan IMT?</h2>
+                    <p>lorem</p>
+                </div>
+
+                <div class="latest-post3">
+                    <h2>Seorang Programmer Terkena Tipes</h2>
+                    <p>lorem</p>
+                </div>
+
             </div>
 
-            <div class="socmed-instagram">
-                <img src="img/instagram.png" class="img-ig">
-                <h1>Instagram</h1>
+            <div class="our-social-media">
+                <h1>OUR SOCIAL MEDIA!</h1>
+
+                <div class="socmed-facebook">
+                    <img src="img/facebook.png" class="img-fb">
+                    <h1>Facebook</h1>
+                </div>
+
+                <div class="socmed-youtube">
+                    <img src="img/youtube.png" class="img-yt">
+                    <h1>Youtube</h1>
+                </div>
+
+                <div class="socmed-instagram">
+                    <img src="img/instagram.png" class="img-ig">
+                    <h1>Instagram</h1>
+                </div>
+
+
             </div>
 
+        </section>
 
-        </div>
-    
-    </section>
-    
-    <!-- <section>
+        <!-- <section>
         <footer>
             <h4>Copyright 2021 | GameInfo</h4>
         </footer>
 
     </section> -->
-
-           
-
+    </div>
+    <div class="clear"></div>
+    <section>
+        <footer>
+            <div class="Footer">
+                <h1 class="adminFooterLogo">GameInfo</h1>
+                <div class="clear"></div>
+                <h4 class="adminFooterLogo2">INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h4>
+            </div>
+            <div class="clear"></div>
+            <br>
+            <hr>
+            <h4>Copyright 2021 | GameInfo</h4>
+        </footer>
+    </section>
 </body>
+
 </html>
