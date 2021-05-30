a4c4cd75a009e96973c8aae2fe521fb7d97e35ad
michaellchandra <76256177+michaellchandra@users.noreply.github.com>
2021-05-30 12:39:30 +0800

    initial commit

--- news-page.php
+++ news-page.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <link rel="stylesheet" href="stylePost.css">
+    <title>"JUDUL HALAMAN POSTINGAN"</title>
 </head>
 <body>
     
+    <div class="isiPost">
+        <h1>Selyandaru Akhirnya Tidak Jomblo</h1>
+        <p class="postDate">25 Mei 2021</p>
+        <p class="adminPost">oleh Admin</p>
+   
+
+
+    </div>
+
+
+
+
 </body>
 </html>
