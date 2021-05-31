439485745696f264febd4ec9f9a72aea1af62217
MakLo1337 <mrwinataa@gmail.com>
2021-05-31 08:03:07 +0700

    login control

--- controller.php
+++ controller.php
@@ -14,6 +14,30 @@
         mysqli_close($x);
     }
 
+    function login(){
+        $adminuser = "admin";
+        $adminpass = "admin";
+
+        session_start();
+
+        $user = isset($_POST['username']) ? $_POST['username'] : "";
+        $pass = isset($_POST['password']) ? $_POST['password'] : "";
+
+        if($user == $adminuser && $pass == $adminpass){
+            session_start();
+            $_SESSION['username'] = $user;
+            header("Location: admin.php");
+        } else {
+            $message = "Invalid Login Credentials!";
+            echo "<script>
+            alert('$message');
+            window.location.href='login.php';
+            </script>";
+            exit;
+            // header("Location: login.php");
+        }
+    }
+
 
     
 ?>
