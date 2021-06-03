63f43ac744522cc4e4055c69fce420f37f2b25e3
UnknownArt01 <sriskanadi@student.ciputra.ac.id>
2021-06-04 06:57:40 +0700

    tampilan mobile part 1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,6 +13,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="all.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
     <title>GameInfo Official Website</title>
 </head>
@@ -20,110 +22,114 @@
     <div class="bodydiv">
         <!--Navigation Bar -->
         <header class="adminHeader">
-            <div class="header">
-                <h1 class="adminHeaderLogo">GameInfo</h1>
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbutton">
+                    <i class="fas fa-bars"></i>
+                </label>
+                <img class="logo" src="img/logo_gameinfo-removebg-preview.png" alt="">
+
+                <ul class="adminHeaderA">
+                    <li><a class="a_header" href="index.php">Home</a></li>
+                    <li><a class="a_header" href="category.php">Category</a></li>
+                    <li><a class="a_header" href="news-page.php">News</a></li>
+                    <li class="a_header-hot"><a href="">HOT NEWS!</a></li>
+                    <li><a class="a_header" href="admin.php">Admin</a></li>
+                </ul>
+            </nav>
+
+            <!-- <div class="adminHeaderA">
+                <a class="a_header" href="index.php">Home</a>
+                <a class="a_header" href="category.php">Category</a>
+                <a class="a_header" href="news-page.php">News</a>
+                <a class="a_header-hot" href="">HOT NEWS!</a>
+                <a class="a_header" href="admin.php">Admin</a>
+            </div> -->
+    </div>
+    </header>
+    <!--Halaman Utama -->
+    <section class="container1">
+        <div class="div1-left">
+
+        </div>
+
+    </section>
+
+    <section class="container2">
+        <div class="HotNews">
+            <div class="BiggestHot"></div>
+            <div class="PhoneHot"></div>
+            <div class="biggesthot2">
+                <div class="SmallHot1">
+
+                </div>
                 <div class="clear"></div>
-                <h4 class="adminHeaderLogo2">INFO MENARIK SEPUTAR GAME & TEKNOLOGI</h4>
-            </div>
-            <div class="a_header">
-            <div class="adminHeaderA">
-                    <a class="a_header" href="index.php">Home</a>
-                    <a class="a_header" href="category.php">Category</a>
-                    <a class="a_header" href="news-page.php">News</a>
-                    <a class="a_header-hot" href="">HOT NEWS!</a>
-                    <a class="a_header" href="admin.php">Admin</a>
-                </div>
-            </div>
-
-            <div class="header">
-                <form action="">
-                    <input class="inputSearch" type="search" name="Search" value="Search">
-                    <input class="buttonSubmit" type="submit" name="search_button" value="search">
-                </form>
-            </div>
-        </header>
-        <!--Halaman Utama -->
-        <section class="container1">
-            <div class="div1-left">
-
-            </div>
-
-        </section>
-
-        <section class="container2">
-            <div class="HotNews">
-                <div class="BiggestHot"></div>
-                <div class="biggesthot2">
-                    <div class="SmallHot1">
-                        
-                    </div>
-                    <div class="clear"></div>
-                    <div class="SmallHot2">
-                        
-                    </div>
-                    <div class="clear"></div>
-                    <div class="SmallHot3">
-                        
-                    </div>
-                </div>
-            </div>
-            <div class="clear"></div>
-            <div class="latest-post">
-                <h1>LATEST POST</h1>
-                <br>
-                <div class="latest-post1">
-                    <div class="postImage"></div>
-                    <div class="ArticleHome">
-                        <h2>Selyandaru Akhirnya Tidak Jomblo</h2>
-                        <p>lorem</p>
-                    </div>
+                <div class="SmallHot2">
 
                 </div>
-
-                <div class="latest-post2">
-                    <div class="postImage"></div>
-                    <div class="ArticleHome">
-                        <h2>Rafi Masuk Nominal Tertampan IMT?</h2>
-                        <p>lorem</p>
-                    </div>
+                <div class="clear"></div>
+                <div class="SmallHot3">
 
                 </div>
-
-                <div class="latest-post3">
-                    <div class="postImage"></div>
-                    <div class="ArticleHome">
-                        <h2>Seorang Programmer Terkena Tipes</h2>
-                        <p>lorem</p>
-                    </div>
-
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="latest-post">
+            <h1>LATEST POST</h1>
+            <br>
+            <div class="latest-post1">
+                <div class="postImage"></div>
+                <div class="ArticleHome">
+                    <h2>Selyandaru Akhirnya Tidak Jomblo</h2>
+                    <p>lorem</p>
                 </div>
 
             </div>
 
-            <div class="our-social-media">
-                <h1>OUR SOCIAL MEDIA!</h1>
-
-                <div class="socmed-facebook">
-                    <div class="img-fb"><img src="img/facebook.png"></div>
-                    <h2 class="socmed">GameInfo</h2>
+            <div class="latest-post2">
+                <div class="postImage"></div>
+                <div class="ArticleHome">
+                    <h2>Rafi Masuk Nominal Tertampan IMT?</h2>
+                    <p>lorem</p>
                 </div>
-
-                <div class="socmed-youtube">
-                    <div class="img-yt"><img src="img/youtube.png"></div>
-                    <h2 class="socmed">GameInfo Official</h2>
-                </div>
-
-                <div class="socmed-instagram">
-                    <div class="img-ig"><img src="img/instagram.png"></div>
-                    <h2 class="socmed">officialgameinfo</h2>
-                </div>
-
 
             </div>
 
-        </section>
+            <div class="latest-post3">
+                <div class="postImage"></div>
+                <div class="ArticleHome">
+                    <h2>Seorang Programmer Terkena Tipes</h2>
+                    <p>lorem</p>
+                </div>
 
-        <!-- <section>
+            </div>
+
+        </div>
+
+        <div class="our-social-media">
+            <h1>OUR SOCIAL MEDIA!</h1>
+
+            <div class="socmed-facebook">
+                <div class="img-fb"><img src="img/facebook.png"></div>
+                <h2 class="socmed">GameInfo</h2>
+            </div>
+
+            <div class="socmed-youtube">
+                <div class="img-yt"><img src="img/youtube.png"></div>
+                <h2 class="socmed">GameInfo Official</h2>
+            </div>
+
+            <div class="socmed-instagram">
+                <div class="img-ig"><img src="img/instagram.png"></div>
+                <h2 class="socmed">officialgameinfo</h2>
+            </div>
+
+
+        </div>
+
+    </section>
+
+    <!-- <section>
         <footer>
             <h4>Copyright 2021 | GameInfo</h4>
         </footer>
@@ -144,5 +150,5 @@
         </footer>
     </section>
 </body>
-</html>
 
+</html>
